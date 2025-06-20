42e6c0416c5c36bb8e254630c36f5fe3ad23d04c
Logan Saso <logan.saso@conductorone.com>
2025-06-20 12:17:38 -0700

    Allow heartbeating a request (#7)
    * Maybe works
    
    * Add heartbeat lua
    
    * Lint complained these aren't relevant anymore

--- concurrency.go
+++ concurrency.go
@@ -46,6 +46,14 @@ func (tk *Limiter) Take(ctx context.Context, key string, requestID string, limit
 	return rv[key], nil
 }
 
+func (tk *Limiter) Heartbeat(ctx context.Context, key string, requestID string, limit ConcurrencyLimit) (ConcurrencyResult, error) {
+	rv, err := tk.heartbeatMulti(ctx, requestID, map[string]ConcurrencyLimit{key: limit}, 0)
+	if err != nil {
+		return ConcurrencyResult{}, err
+	}
+	return rv[key], nil
+}
+
 func (p *pipeline) takePipe(ctx context.Context, pipe redis.Pipeliner, rv *ConcurrencyResult) func() error {
 	p.buf.Reset()
 	_, _ = p.buf.WriteString(p.l.concurrentPrefix)
@@ -59,6 +67,35 @@ func (p *pipeline) takePipe(ctx context.Context, pipe redis.Pipeliner, rv *Concu
 	values := []interface{}{rv.RequestID, rv.Limit.Max, reqPeriod}
 
 	eval := concurrencyTake.EvalSha(ctx, pipe, []string{p.buf.String()}, values...)
+	return func() error {
+		v, err := eval.Result()
+		if err != nil {
+			return err
+		}
+		values := v.([]interface{})
+
+		ok := values[0].(int64) == 1
+		current := values[1].(int64)
+		rv.Allowed = ok
+		rv.Used = current
+		rv.Remaining = rv.Limit.Max - current
+		return nil
+	}
+}
+
+func (p *pipeline) heartbeatPipe(ctx context.Context, pipe redis.Pipeliner, rv *ConcurrencyResult) func() error {
+	p.buf.Reset()
+	_, _ = p.buf.WriteString(p.l.concurrentPrefix)
+	_, _ = p.buf.WriteString(rv.Key)
+
+	reqPeriod := rv.Limit.RequestMaxDuration.Round(time.Second) / time.Second
+	if reqPeriod <= 0 {
+		reqPeriod = 60
+	}
+
+	values := []interface{}{rv.RequestID, reqPeriod}
+
+	eval := concurrencyHeartbeat.EvalSha(ctx, pipe, []string{p.buf.String()}, values...)
 	return func() error {
 		v, err := eval.Result()
 		if err != nil {
@@ -175,6 +212,91 @@ func (tk *Limiter) takeMulti(ctx context.Context, requestID string, limits map[s
 			return nil, err
 		}
 		return tk.takeMulti(ctx, requestID, limits, depth+1)
+	}
+
+	rv := make(map[string]ConcurrencyResult, len(results))
+	for _, result := range results {
+		v, err := result.cmd.Result()
+		if err != nil {
+			return nil, err
+		}
+		values := v.([]interface{})
+
+		ok := values[0].(int64) == 1
+		current := values[1].(int64)
+		cr := ConcurrencyResult{
+			RequestID: requestID,
+			Key:       result.key,
+			Allowed:   ok,
+			Limit:     result.limit,
+			Used:      current,
+			Remaining: result.limit.Max - current,
+		}
+		rv[result.key] = cr
+	}
+
+	return rv, nil
+}
+
+type heartbeatResult struct {
+	key   string
+	limit ConcurrencyLimit
+	cmd   *redis.Cmd
+}
+
+func (tk *Limiter) heartbeatMulti(ctx context.Context, requestID string, limits map[string]ConcurrencyLimit, depth int) (map[string]ConcurrencyResult, error) {
+	if depth > 10 {
+		return nil, ErrTooManyRetries
+	}
+
+	results := make([]*heartbeatResult, 0, len(limits))
+	buf := bytes.Buffer{}
+	pl := tk.rdb.Pipeline()
+	existsCmd := concurrencyHeartbeat.Exists(ctx, pl)
+	for key, limit := range limits {
+		reqPeriod := limit.RequestMaxDuration.Round(time.Second) / time.Second
+		if reqPeriod <= 0 {
+			reqPeriod = 60
+		}
+		values := []interface{}{requestID, reqPeriod}
+
+		buf.Reset()
+		_, _ = buf.WriteString(tk.concurrentPrefix)
+		_, _ = buf.WriteString(key)
+
+		results = append(results, &heartbeatResult{
+			key:   key,
+			limit: limit,
+			cmd: concurrencyHeartbeat.EvalSha(
+				ctx,
+				pl,
+				[]string{buf.String()},
+				values...,
+			),
+		})
+	}
+	if len(results) == 0 {
+		return nil, nil
+	}
+	_, err := pl.Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	exists, err := existsCmd.Result()
+	if err != nil {
+		return nil, err
+	}
+	if len(exists) != 1 {
+		return nil, ErrScriptFailed
+	}
+
+	if !exists[0] {
+		err = tk.LoadScripts(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return tk.heartbeatMulti(ctx, requestID, limits, depth+1)
 	}
 
 	rv := make(map[string]ConcurrencyResult, len(results))
